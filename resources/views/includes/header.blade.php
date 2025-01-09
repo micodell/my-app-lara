3e4d27abd5c20b45b0d0f6cb6f5ad5bcc022af57
micodell <michellenathaniawinata@gmail.com>
2025-01-09 17:30:56 +0700

    Fixing: header shadow

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,4 +1,4 @@
-<nav id="header" class="bg-white bg-opacity-0 transition-all duration-500 ease-in-out text-slate-300 p-4 h-20 flex items-center sticky top-0 z-50">
+<nav id="header" class="bg-white bg-opacity-0 shadow-md transition-all duration-400 ease-in-out text-slate-300 p-4 h-20 flex items-center sticky top-0 z-50">
     <div class="flex flex-row justify-between w-full mx-4">
         <a href="" class="font-bold text-2xl hover:scale-105 transition-all duration-500 ease-in-out">GENTA UNIVERSITY</a>
         <a href="mailto:c14240169@john.petra.ac.id" 
@@ -12,10 +12,10 @@
     window.addEventListener('scroll', ()=> {
         if (window.scrollY > 0) {
             header.classList.remove('bg-opacity-0');
-            header.classList.add('backdrop-blur-lg', 'shadow-md', 'bg-opacity-5');
+            header.classList.add('backdrop-blur-lg', 'bg-opacity-5');
         } else {
             header.classList.add('bg-opacity-0');
-            header.classList.remove('backdrop-blur-lg', 'shadow-md', 'bg-opacity-5');
+            header.classList.remove('backdrop-blur-lg', 'bg-opacity-5');
         }
     })
 </script>
