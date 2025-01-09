5c936847fdfaeecb2b38da92d8f32b2d96cc898f
micodell <michellenathaniawinata@gmail.com>
2025-01-09 14:39:52 +0700

    Fixing: header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,5 +1,21 @@
-<nav class="bg-white bg-opacity-5 text-slate-300 p-4 h-20 flex items-center">
-    <div class="mx-4">
-        <h1 class="font-bold text-2xl">GENTA UNIVERSITY</h1>
+<nav id="header" class="bg-white bg-opacity-0 transition-all duration-500 ease-in-out text-slate-300 p-4 h-20 flex items-center sticky top-0 z-50">
+    <div class="flex flex-row justify-between w-full mx-4">
+        <a href="" class="font-bold text-2xl hover:scale-105 transition-all duration-500 ease-in-out">GENTA UNIVERSITY</a>
+        <a href="mailto:c14240169@john.petra.ac.id" 
+        class="font-semibold text-xl hover:text-blue-400 hover:border-b-2 border-blue-400 transition-all duration-100 ease-in-out">Contact</a>
     </div>
 </nav>
+
+<script>
+    const header = document.getElementById('header');
+
+    window.addEventListener('scroll', ()=> {
+        if (window.scrollY > 0) {
+            header.classList.remove('bg-opacity-0');
+            header.classList.add('backdrop-blur-lg', 'shadow-md', 'bg-opacity-5');
+        } else {
+            header.classList.add('bg-opacity-0');
+            header.classList.remove('backdrop-blur-lg', 'shadow-md', 'bg-opacity-5');
+        }
+    })
+</script>
